0a7aba7af568e867c56cc69e09e52892b5bb21cd
GF603L9 <ashish.sawant@teradata.com>
2023-12-26 19:28:48 +0530

    Updated test.py file

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pandas as pd
 from datetime import datetime, timedelta
 
-
-excel_file = 'CMP_data.xlsx'
+excel_file = '../CMP_data.xlsx'
 df = pd.read_excel(excel_file)
 
+################### Current Date ################
+year = int(input('Enter a year: '))
+month = int(input('Enter a month: '))
+day = int(input('Enter a day: '))
 
-current_date = datetime(2023, 10, 31)
+
+current_date = datetime(year, month,day)
 
 
 less_than_7_days = []
@@ -15,12 +19,15 @@ between_30_and_60_days = []
 between_60_and_90_days = []
 greater_than_90_days = []
 
+count = 0
+
 
 for index, row in df.iterrows():
-    open_date = row['Created']
+    count += 1
+    open_date = row['Created'] 
     open_date_1 = pd.to_datetime(open_date)
     open_date = open_date_1.date()
-    print(open_date)
+    # print(open_date)
     # days_open = (current_date - open_date).days
 
     age = 0
@@ -29,9 +36,9 @@ for index, row in df.iterrows():
         if temp_date.weekday() < 5:
             age += 1
         temp_date += timedelta(days=1)
-    print(age)
+    # print(age)
     if age <= 7:
-        less_than_7_days.append(row['Key'])
+        less_than_7_days.append(row["Key"])
     elif 7 < age <= 30:
         between_7_and_30_days.append(row['Key'])
     elif 30 < age <= 60:
@@ -50,7 +57,7 @@ print("Tickets open greater than 90 days:", greater_than_90_days)
 
 
 
-print("Tickets Worked",  	49)
+print("Unresolved ticket Count", count)
 print("# of Ticket Backlog(<7 Days) ", len(less_than_7_days))
 print("# of Ticket Backlog(GT_7D_LT_30D)", 	len(between_7_and_30_days))
 print("# of Ticket Backlog(GT_30D_LT_60D)", len(between_30_and_60_days)) 	
